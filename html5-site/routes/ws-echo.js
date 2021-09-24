3ddf6c4dba92b3cfb5eac5dc49904bcafeaaa221
story <story801216@yahoo.com.tw>
2021-09-25 02:03:18 +0800

    WS : Echo client

--- html5-site/routes/ws-echo.js
+++ html5-site/routes/ws-echo.js
@@ -4,7 +4,7 @@ const createEchoServer =server => {
     const wsServer = new WebSocket.Server({server});
 
     wsServer.on('connection', (ws,req) => {  // 當有人連線時候要做的事情
-        console.log('size', wsServer.client.size);
+        console.log('size:', wsServer.clients.size);
         console.log('ip:', req.connection.remoteAddress);
 
         ws.on('message', message=>{
